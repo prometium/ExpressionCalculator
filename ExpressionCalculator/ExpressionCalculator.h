b134a17e5b1bb25351f57a6a25d442ad47b7b9b5
prometium <prometium@users.noreply.github.com>
2017-12-07 23:02:26 +0300

    upd

--- ExpressionCalculator/ExpressionCalculator.h
+++ ExpressionCalculator/ExpressionCalculator.h
@@ -2,7 +2,4 @@
 
 #pragma once
 
-// param expression Math expression.
-// param angularMeasure Angular measure (0 - DEG, 1 - RAD).
-// return Result of math expression.
-extern "C" __declspec(dllexport) double Calculate(char* expression, const bool angularMeasure);
+extern "C" __declspec(dllexport) double Calculate(char* expression)
